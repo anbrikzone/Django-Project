f51503169063aa4eefd1da645eaaa3d3f93e0ba1
Anatoliy <anbrikzone@gmail.com>
2024-03-02 22:40:01 +0500

    add some static sources

--- collaborate_project/authorization/models.py
+++ collaborate_project/authorization/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+
+    login       = models.CharField(max_length=64)
+    password    = models.CharField(max_length=256)
+    first_name  = models.CharField(max_length=128)
+    last_name   = models.CharField(max_length=128)
+    age         = models.SmallIntegerField()
+    email       = models.EmailField()
+
--- collaborate_project/authorization/views.py
+++ collaborate_project/authorization/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# This function shows main page
+# If user is not authorized function return login/registraion form
+# if user is authorized than function redirect him to projects index page
+def index(request):
+    return render(request, "authorization/index.html")
+
+def signup(request):
+    return render(request, "authorization/signup.html")
+
+def login(request):
+    return render(request, "authorization/login.html")
