510d5d75f8b36a7d2f0ac70c789ec3213000a2c6
Anatoliy <anbrikzone@gmail.com>
2024-03-02 23:29:43 +0500

    add models for authorization app

--- collaborate_project/authorization/models.py
+++ collaborate_project/authorization/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class Users(models.Model):
+class Roles(models.Model):
+    type        = models.CharField(max_length=64)
 
+class Users(models.Model):
     login       = models.CharField(max_length=64)
     password    = models.CharField(max_length=256)
     first_name  = models.CharField(max_length=128)
     last_name   = models.CharField(max_length=128)
     age         = models.SmallIntegerField()
     email       = models.EmailField()
-
+    roles       = models.ForeignKey(Roles, on_delete=models.DO_NOTHING)
