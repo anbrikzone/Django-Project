91016e961618db66d10a3f3f05acb048cbf6e06f
Anatoliy <anbrikzone@gmail.com>
2024-03-02 16:46:30 +0500

    rename project

--- collaborate_project/collaborate_project/asgi.py
+++ collaborate_project/collaborate_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for collaborate project.
+ASGI config for collaborate_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborate_project.settings')
 
 application = get_asgi_application()
