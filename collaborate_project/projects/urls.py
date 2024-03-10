5e6c7c4b0b5ccc41009c4efade244d32d9402c61
Anatoliy <anbrikzone@gmail.com>
2024-03-11 00:23:45 +0500

    add removing project's member & resolve issue with raw sql

--- collaborate_project/projects/urls.py
+++ collaborate_project/projects/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add_project/', views.add_project, name='add_project'),
     path('edit_project/<int:id>', views.edit_project, name='edit_project'),
     path('remove_project/<int:id>', views.remove_project, name='remove_project'),
+    path('remove_projectmember/<int:id>', views.remove_projectmember, name='remove_projectmember'),
 ]
--- collaborate_project/projects/views.py
+++ collaborate_project/projects/views.py
@@ -13,16 +13,18 @@ def index(request):
     shared_projects = Project.objects.filter(projectmembers__users__id = request.user.id).order_by('-created')
     # my_projects = Project.objects.filter(owner_id = request.user.id).order_by('-created')
 
-    my_projects = Project.objects.raw(f'''select
+    my_projects = Project.objects.raw(f'''select 
                 pp.id
                 , pp.name
                 , pp.description
+                , owner_id
                 , pp.created
-                , group_concat(au.username, ', ') as users 
+                , group_concat(au.username, ', ') as users
                 from projects_project pp 
-                left join projects_projectmember ppm on pp.id = ppm.projects_id 
-                left join auth_user au ON au.id = ppm.users_id
-                where pp.owner_id = %s''', [request.user.id])
+                left join projects_projectmember ppm on pp.id = ppm.projects_id
+                left join auth_user au on au.id = ppm.users_id
+                where pp.owner_id = %s
+                group by pp.id''', [request.user.id])
     
     context = {
         'title': "Projects",
@@ -54,25 +56,40 @@ def add_project(request):
 
 @login_required(login_url="/signin")
 def edit_project(request, id):
-
     if request.method == 'POST':
-        project_name = request.POST.get("project_name")
-        project_description = request.POST.get("project_description")
+        if request.POST.get("EditProject"):
+            project_name = request.POST.get("project_name")
+            project_description = request.POST.get("project_description")
 
-        form = Project.objects.get(pk = id)
-        form.name = project_name
-        form.description = project_description
-        form.save()
+            form = Project.objects.get(pk = id)
+            form.name = project_name
+            form.description = project_description
+            form.save()
 
-        return redirect("index")
-
+            return redirect("index")
+        
+        elif request.POST.get("RemoveMember"):
+            member_id = request.POST.get("select_members")
+            ProjectMember.objects.filter(projects__id = id, users__id = member_id).delete()
+            return redirect(request.META.get('HTTP_REFERER'))
+    
     project = Project.objects.get(pk = id)
+    members = User.objects.filter(projectmember__projects__id = id).values_list('id', 'username')
     context = {
         'title': "Projects",
         'header': "Projects",
         'project': project,
+        'members': members,
         }
     return render(request, 'projects/edit_project.html', context=context)
+
+@login_required(login_url="/signin")
+def remove_projectmember(request, id):
+    if request.method == 'POST':
+        member_id = request.POST.get("select_members")
+        print(member_id)
+        ProjectMember.objects.filter(projects__id = id, users__id = member_id).delete()
+        return redirect("index")
 
 @login_required(login_url="/signin")
 def remove_project(request, id):
