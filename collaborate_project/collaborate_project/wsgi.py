d80d9cceac52db564dd19bdac5927e5a45c0b227
Anatoliy <anbrikzone@gmail.com>
2024-03-26 22:43:31 +0500

    update for Azure production

--- collaborate_project/collaborate_project/wsgi.py
+++ collaborate_project/collaborate_project/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborate_project.settings')
+settings_module = 'collaborate_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'collaborate_project.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
